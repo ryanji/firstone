54765dfb4eb13a5f587fccadc68598713f2561c2
Ryan <x.ryan.ji@gmail.com>
2020-07-29 20:31:00 -0700

    first commit

---
+++ R/hello.R
@@ -0,0 +1,13 @@
+#' @title  My Hello World Function
+#' @param x The name of the person to say hi to
+#' @return  The output from \code{\link{print}}
+#' @export
+#'
+#' @examples
+#' hello("John")
+#' \dontrun{
+#' hello("Ryan")
+#' }
+hello <- function(x) {
+  print(paste0("Hello ", x, ",This is world!"))
+}
